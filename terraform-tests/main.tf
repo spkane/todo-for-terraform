49f0a106e02cfd089e4f608ab7fbcb600abc9361
Sean P. Kane <skane@newrelic.com>
2020-01-01 13:10:04 -0800

    Minor tweaks

--- terraform-tests/main.tf
+++ terraform-tests/main.tf
@@ -5,18 +5,18 @@ provider "todo" {
   schema = "http"
 }
 
+data "todo" "foreign" {
+  id = 1
+}
+
 resource "todo" "test1" {
   count = 5
   description = "${count.index}-1 test todo"
   completed = false
 }
 
-data "todo" "foreign" {
-  id = 1
-}
-
 resource "todo" "test2" {
-  count = 7
+  count = 5
   description = "${count.index}-2 test todo (linked to ${data.todo.foreign.id})"
   completed = false
 }
