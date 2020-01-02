5ba77eab4367fd9d357528ec112dbde777fd2d0d
Sean P. Kane <skane@newrelic.com>
2020-01-01 21:36:44 -0800

    ensure we test with newly built provider

--- scripts/tests_manual.sh
+++ scripts/tests_manual.sh
@@ -10,9 +10,11 @@ docker-compose up -d
 # Add something to import as a data source
 curl -i http://127.0.0.1:8080/ -X POST -H 'Content-Type: application/spkane.todo-list.v1+json' -d '{"description":"go shopping","completed":false}'
 cd terraform-tests
-#rm -rf .terraform/ terraform.tfstate* terraform-provider-todo_v*
+rm -f terraform-provider-todo
+cp ../terraform-provider-todo/bin/terraform-provider-todo .
 ${TF} init --get --upgrade=true
 TF_LOG=debug ${TF} apply
 curl -i http://127.0.0.1:8080/
 docker-compose down
+rm -f terraform-provider-todo
 
