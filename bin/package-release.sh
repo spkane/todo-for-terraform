10c53d1fda117c48aacc11cd7c733514a97ff322
Sean P. Kane <skane@newrelic.com>
2020-10-10 23:43:53 -0700

    Add release packaging script

---
+++ bin/package-release.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -eu
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+cd "$DIR/../cmd/todo-list-server/pkg/"
+
+rm -f *.tar.gz
+
+for i in $(ls -C1 .); do
+  cd ${i}
+  tar -cvzf ../${i}-todo_list_server.tar.gz *
+  cd ..
+done
+
+cd "$DIR/../terraform-provider-todo/pkg/"
+
+rm -f *.tar.gz
+
+for i in $(ls -C1 .); do
+  cd ${i}
+  tar -cvzf ../${i}-todo_list_server.tar.gz *
+  cd ..
+done
+
+exit 0
