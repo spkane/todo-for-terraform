3fa3988d98a94a6d2e3de512e15f29cd8229eb3a
Sean P. Kane <skane@newrelic.com>
2020-10-27 20:23:50 -0700

    Add sleep in test script

--- bin/tests_manual.sh
+++ bin/tests_manual.sh
@@ -19,6 +19,7 @@ cd "$DIR/.."
 ./bin/build.sh
 docker-compose up -d
 # Add something to import as a data source
+sleep 2
 curl -i http://127.0.0.1:8080/ -X POST -H 'Content-Type: application/spkane.todo-list.v1+json' -d '{"description":"go shopping","completed":false}'
 cd terraform-tests
 rm -f terraform-provider-todo
