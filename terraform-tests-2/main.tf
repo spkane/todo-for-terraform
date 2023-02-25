1a49dfd88ecfced3db1ca561f2ba165259410d79
Sean P. Kane <spkane00@gmail.com>
2023-02-24 18:47:53 -0800

    Add second set of tests

---
+++ terraform-tests-2/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    todo = {
+      source  = "spkane/todo"
+      version = "2.0.3"
+    }
+  }
+}
+
+provider "todo" {
+  host = "todo-api.spkane.org"
+  port = "8080"
+  apipath = "/"
+  schema = "http"
+}
+
+resource "todo_todo" "step_2" {
+  count = 5
+  description = "${count.index}: ${var.purpose} todo"
+  completed = false
+}
---
+++ terraform-tests-2/outputs.tf
@@ -0,0 +1,3 @@
+output "todo_ids" {
+  value = todo_todo.step_2.*.id
+}
