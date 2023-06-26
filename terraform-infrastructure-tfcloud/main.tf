d4cf574a89f84c8742f231b2764cccd271ae6b7a
Sean P. Kane <spkane00@gmail.com>
2023-06-26 08:40:11 -0700

    remove S3 backend for TFE code

--- terraform-infrastructure-tfcloud/main.tf
+++ terraform-infrastructure-tfcloud/main.tf
@@ -6,16 +6,6 @@ terraform {
       version = "~> 0.42.0"
     }
   }
-  backend "s3" {
-    profile        = "spkane-training"
-    bucket         = "spkane-training-tfstate"
-    key            = "classes/terraform-301-tfcloud.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    kms_key_id     = "arn:aws:kms:us-east-1:929892624845:key/b8c00916-26bd-4388-be96-afce8110cd98"
-    acl            = "private"
-    dynamodb_table = "tfstate"
-  }
 }
 
 provider "tfe" {
